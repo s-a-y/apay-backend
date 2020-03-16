4c36c3ecc54ff8fe99d7d5978e0fe1dbc6dc3909
otokarev <otokarev@gmail.com>
2020-03-16 16:38:25 +0700

    [*] job-queue.processor.ts: syncDailyBalances() simplified

--- src/job-queue.processor.ts
+++ src/job-queue.processor.ts
@@ -18,75 +18,16 @@ export class JobQueueProcessor {
 
   @Process('syncDailyBalances')
   async syncDailyBalances(job: Job<SyncDailyBalancesDto>) {
-    this.logger.log('syncDailyBalances(): started');
-    return new Promise(async (resolve, reject) => {
-      this.logger.log('syncDailyBalances(): extractBalanceMutations job added');
-      const balanceMutationJob = await this.queue.add('extractBalanceMutations', job.data);
-      this.queue
-        .on("completed", async (job, result) => {
-          if (balanceMutationJob.id === job.id) {
-            this.logger.log('syncDailyBalances(): extractBalanceMutations completed');
-            job.remove();
-            const dailyBalanceJob = await this.queue.add('extractDailyBalances', job.data);
-            this.logger.log('syncDailyBalances(): extractDailyBalances job added');
-            this.queue
-              .on("completed", async (job, result) => {
-                if (dailyBalanceJob.id === job.id) {
-                  this.logger.log('syncDailyBalances(): extractDailyBalances completed');
-                  job.remove();
-                  resolve();
-                }
-              })
-              .on("error", async (error) => {
-                this.logger.error(error);
-                await dailyBalanceJob.remove();
-                this.logger.log('syncDailyBalances(): extractDailyBalances got error');
-                reject(error);
-              })
-              .on("failed", async (job, error) => {
-                if (balanceMutationJob.id === job.id) {
-                  this.logger.error(error);
-                  job.remove();
-                  this.logger.log('syncDailyBalances(): extractDailyBalances failed');
-                  reject(error);
-                }
-              });
-          }
-        })
-        .on("error", async (error) => {
-          this.logger.error(error);
-          await balanceMutationJob.remove();
-          this.logger.log('syncDailyBalances(): extractBalanceMutations got error');
-          reject(error);
-        })
-        .on("failed", async (job, error) => {
-          if (balanceMutationJob.id === job.id) {
-            this.logger.error(error);
-            job.remove();
-            this.logger.log('syncDailyBalances(): extractBalanceMutations failed');
-            reject(error);
-          }
-        });
-    });
-  }
-
-  @Process('extractBalanceMutations')
-  async extractBalanceMutations(job: Job<SyncDailyBalancesDto>) {
-    await this.balanceMutationExtractorService.extract({
+    return this.balanceMutationExtractorService.extract({
       toDate: new Date(job.data.toDate),
       mode: ExtractBalanceMutationMode.FROM_TAIL,
       accountId: job.data.accountId,
+    }).then(() => {
+      return this.dailyBalanceExtractorService.extract({
+        toDate: job.data.toDate,
+        mode: ExtractDailyBalanceMode.FROM_TAIL,
+        accountId: job.data.accountId,
+      });
     });
-    return {}
-  }
-
-  @Process('extractDailyBalances')
-  async extractDailyBalances(job: Job<SyncDailyBalancesDto>) {
-    await this.dailyBalanceExtractorService.extract({
-      toDate: job.data.toDate,
-      mode: ExtractDailyBalanceMode.FROM_TAIL,
-      accountId: job.data.accountId,
-    });
-    return {}
   }
 }
