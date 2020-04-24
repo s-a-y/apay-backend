ae2695933737ee471bb1580b2eb82af8cfcd99a2
umbrel <s@apay.io>
2020-04-24 16:09:07 +1000

    Add USDT to supported currencies

--- src/app.enums.ts
+++ src/app.enums.ts
@@ -2,17 +2,15 @@ export enum SupportedCurrency {
     BTC = 'BTC',
     XDR = 'XDR',
     XLM = 'XLM',
-    BCH = 'BCH',
     ETH = 'ETH',
     LTC = 'LTC',
     BAT = 'BAT',
     KIN = 'KIN',
     LINK = 'LINK',
-    MTL = 'MTL',
     OMG = 'OMG',
     REP = 'REP',
-    SALT = 'SALT',
-    ZRX = 'ZRX'
+    ZRX = 'ZRX',
+    USDT = 'USDT',
 }
 
 /**
@@ -33,8 +31,8 @@ export enum JobState {
     failed = 'failed',
 }
 export enum OrderOption {
-    ASC = "ASC",
-    DESC = "DESC"
+    ASC = 'ASC',
+    DESC = 'DESC',
 }
 
 export enum BalanceMutationType {
